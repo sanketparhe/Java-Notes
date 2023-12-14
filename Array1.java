2e3ae91b6132040f99143372251cb148539e0ad9
sanketparhe <139855581+sanketparhe@users.noreply.github.com>
2023-12-14 15:07:43 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,31 @@
+class Array1 {
+    public static void main(String[] args) {
+        //  int[] arr = { 10, 20, 30, 40,50};
+
+        // System.out.println(arr[4]);
+        // System.out.println(arr[3]);
+        // System.out.println(arr[2]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[0]);
+        // System.out.println(arr.length);
+
+        // System.out.println(arr[1]+arr[3]);
+
+        // int [] arr1 = new int[]{10,20,30,40,100};
+        // System.out.println(arr1);
+        // System.out.println(arr1[2]);
+        // System.out.println(arr1.length);
+
+        // int sum=0;
+        // for(int i=0; i<arr.length; i++){
+        //     if (arr[i]%2==0){
+        //         sum=sum+arr[i];
+        //     }
+        //     System.out.println(sum);
+        // }
+            
+
+
+
+    }
+}
