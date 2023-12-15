154b6fd78614e4e7d78202ea48a0747214682ea3
sanketparhe <139855581+sanketparhe@users.noreply.github.com>
2023-12-15 15:58:17 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Array2 {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Value");
+      int a=sc.nextInt();
+      int arr[]= new int[a];
+      for(int i=0; i<arr.length; i++){
+        System.out.println(arr[i]);
+      }
+    }
+}
